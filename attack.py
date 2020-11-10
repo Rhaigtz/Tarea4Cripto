cee13fb2a5a12e2e09bab486900f554b8de2fd6c
Rhaigtz <nopazog@gmail.com>
2020-11-10 19:15:27 -0300

    First commit

---
+++ attack.py
@@ -0,0 +1,103 @@
+
+import bcrypt
+import os
+import base64
+import socket
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import codecs
+import time
+
+
+hashes = [{"name": 'md5', "code": '0'}, {"name": 'md5_static_salt', "code": '10'}, {
+    "name": 'md5_dynamic_salt', 'code': '10'}, {"name": 'ml', "code": '1000'}, {'name': 'sha512_crypt', 'code': '1800'}]
+
+print('Bienvenido al menu de des-encriptado de hashes, seleccione el numero del archivo que desee des-encriptar:', "\n")
+
+print('1. MD5.txt\n')
+print('2. MD5_STATIC_SALT.txt\n')
+print('3. MD5_DYNAMIC_SALT.txt\n')
+print('4. ML.txt\n')
+print('5. SHA512_CRYPT.txt\n')
+
+hash_type = int(input(''))
+
+while((bool(hash_type != 1) ^ bool(hash_type != 2) ^ bool(hash_type != 3) ^ bool(hash_type != 4) ^ bool(hash_type != 5))):
+    print('Opcion no valida, porfavor intente nuevamente.\n')
+    print('Bienvenido al menu de des-encriptado de hashes, seleccione el numero del archivo que desee des-encriptar:', "\n")
+    print('1. MD5.txt\n')
+    print('2. MD5_STATIC_SALT.txt\n')
+    print('3. MD5_DYNAMIC_SALT.txt\n')
+    print('4. ML.txt\n')
+    print('5. SHA512_CRYPT.txt\n')
+
+    hash_type = int(input(''))
+
+hash_type = hashes[hash_type - 1]
+
+
+os.system('cd hashcat & hashcat.exe -m {} -a 0 Hashes\{}.txt diccionarios\diccionario_2.txt --force --outfile=../{}_text.txt --outfile-format 2'.format(
+    hash_type["code"], hash_type["name"], hash_type['name']))
+# if(hash_type == 1):
+#     os.system('cd hashcat & hashcat.exe -m 0 -a 0 Hashes\md5.txt diccionarios\diccionario_2.txt --force --outfile=../md5_text.txt --outfile-format 2')
+# elif(hash_type == 2):
+#     os.system('cd hashcat & hashcat.exe -m 10 -a 0 Hashes\md5_static_salt.txt diccionarios\diccionario_2.txt --force --outfile=../md5_static_text.txt --outfile-format 2')
+# elif(hash_type == 3):
+#     os.system('cd hashcat & hashcat.exe -m 10 -a 0 Hashes\md5_dynamic_salt.txt diccionarios\diccionario_2.txt --force --outfile=../md5_dynamic_text.txt --outfile-format 2')
+# elif(hash_type == 4):
+#     os.system('cd hashcat & hashcat.exe -m 1000 -a 0 Hashes\ml.txt diccionarios\diccionario_2.txt --force --outfile=../ml_text.txt --outfile-format 2')
+# elif(hash_type == 5):
+#     os.system('cd hashcat & hashcat.exe -m 1800 -a 0 Hashes\sha-512-crypt.txt diccionarios\diccionario_2.txt --force --outfile=../sha-512_text.txt --outfile-format 2')
+
+
+fileHash = open("{}_text.txt".format(hash_type['name']), "r")
+
+
+hash_files = input('Desea hashear los archivos con bcrypt? Y/N\n')
+
+if hash_files == 'Y':
+    file_object = open("{}_hashed.txt".format(hash_type['name']), "w")
+    start_time = time.time()
+    for hash in fileHash:
+        pwd = bytes(hash, 'utf-8')
+        hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
+        hashed = hashed.decode('utf-8')
+        hashed += '{}\n'.format(hashed)
+        file_object.write(hashed)
+    file_object.write("--- %s seconds ---" % (time.time() - start_time))
+    file_object.close()
+
+fileHash.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 3000))
+
+
+while True:
+    s.sendall(bytes(hash_type['name'], 'utf-8'))
+    full_msg = input('Ingrese su mensaje hacia el server\n')
+    s.sendall(bytes(full_msg, 'utf-8'))
+
+    if full_msg == 'exit':
+        break
+
+    elif full_msg == 'key':
+        # hashedFile = open('md5_hashed.txt', 'r')
+        received_message = s.recv(1024)
+        new_file = open("{}_rehashed.txt".format(hash_type['name']), "w")
+        file_with_hashes = open('{}_hashed.txt'.format(hash_type['name']), 'r')
+        key = RSA.importKey(open(received_message.decode('utf-8')).read())
+
+        cipher = PKCS1_OAEP.new(key)
+        for hash in file_with_hashes:
+            message = bytes(hash, 'utf-8')
+            ciphertext = cipher.encrypt(message)
+            new_file.write(ciphertext.hex().upper()+'\n')
+        file_with_hashes.close()
+        new_file.close()
+        s.sendall(bytes('{}_rehashed.txt'.format(hash_type['name']), 'utf-8'))
+
+    else:
+        received_message = s.recv(1024)
+        print(received_message.decode('utf-8'))
+s.close()
---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import codecs
+
+hashes = [{"name": 'md5', "code": '0'}, {"name": 'md5_static_salt', "code": '10'}, {
+    "name": 'md5_dynamic_salt', 'code': '10'}, {"name": 'ml', "code": '1000'}, {'name': 'sha512_crypt', 'code': '1800'}]
+
+key = RSA.generate(2048)
+
+private_key = key.export_key()
+file_out = open("private.pem", "wb")
+file_out.write(private_key)
+file_out.close()
+
+public_key = key.publickey()
+
+exported_key = public_key.export_key()
+array_key = exported_key.decode('utf-8').split('\n')
+cuted_key = ''
+for x in range(1, len(array_key) - 1):
+    cuted_key += array_key[x]
+
+file_out = open("receiver.pem", "wb")
+file_out.write(exported_key)
+file_out.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 3000))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.listen(1)
+clientsocket, address = s.accept()
+
+file_hash = clientsocket.recv(300).decode('utf-8')
+while True:
+    try:
+        print("connected from ", address)
+        received_message = clientsocket.recv(300).decode('utf-8')
+        if received_message == 'exit':
+            break
+        elif received_message == 'key':
+            print('entre a key')
+            clientsocket.sendall(bytes('receiver.pem', 'utf-8'))
+            hashed_file_route = clientsocket.recv(300).decode('utf-8')
+
+            hashed_file = open(hashed_file_route, 'r')
+
+            unhashed_file = open('{}_unhashed'.format(file_hash), 'w')
+            key = RSA.importKey(open('private.pem').read())
+            cipher = PKCS1_OAEP.new(key)
+            for hash_line in hashed_file:
+                message = cipher.decrypt(bytes.fromhex(hash_line))
+                unhashed_file.write(message.decode('utf-8'))
+            unhashed_file.close()
+            hashed_file.close()
+
+    except KeyboardInterrupt:
+        if clientsocket:  # <---
+            clientsocket.close()
+        break  # <---
+s.close()
+clientsocket.close()
